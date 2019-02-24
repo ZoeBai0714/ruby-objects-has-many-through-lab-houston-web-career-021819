3bd50df6dde4bf90bb33d27a6bb808d376be1b02
Zoe Bai <s.xue.bai@gmail.com>
2019-02-24 03:34:11 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,17 @@
+class Artist
+    @@all = []
+  attr_accessor :name
+  def initialize(name)
+    @name = name
+    @songs = []
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def new_song(name, genre)
+    song = Song.new(name, self, genre)
+  end
+end
---
+++ lib/genre.rb
@@ -0,0 +1,13 @@
+class Genre
+  @@all = []
+  attr_accessor :name, :songs
+  def initialize(name)
+    @name = name
+    @songs = []
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+end
