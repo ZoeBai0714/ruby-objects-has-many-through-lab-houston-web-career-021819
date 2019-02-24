4dcbd985211774713afd3982c3ce53f3dfd61757
Zoe Bai <s.xue.bai@gmail.com>
2019-02-24 04:25:12 +0000

    Automatically backed up by Learn

---
+++ lib/appointment.rb
@@ -0,0 +1,13 @@
+class Appointment
+  @all = []
+  
+  
+  def initialize(date, patient, doctor)
+    @date = date
+    @patient = patient
+    @doctor = doctor
+  end
+  def self.all
+    @@all
+  end
+end
